4a8704420a3bcadde2d7212b054297720ab4e040
Nils Maier <maierman@web.de>
2009-04-02 18:02:51 +0000

    * scripts to generate dta filters  * fixes for some plugins

---
+++ json-to-filters.py
@@ -0,0 +1,18 @@
+import os, re, sys
+import fileinput
+from mergeex import mergeex
+import simplejson as json
+from glob import glob
+
+filters = ()
+for js in glob("plugins/*.json"):
+        f = open(js, 'r')
+        js = json.load(f, "utf-8")
+        filters += js['match'],
+        f.close()
+
+filters = mergeex(map(lambda x: re.sub(r'/', '\\/', x), filters))
+print r'pref("extensions.dta.filters.deffilter-ca.label", "AntiContainer");'
+print r'pref("extensions.dta.filters.deffilter-ca.test", "/' +  filters + '/i");'
+print r'pref("extensions.dta.filters.deffilter-ca.active", true);'
+print r'pref("extensions.dta.filters.deffilter-ca.type", 1);'
