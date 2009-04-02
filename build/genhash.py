bec02d65cc2521080ba1e06241f8de1087dbb505
Nils Maier <maierman@web.de>
2009-04-02 20:50:05 +0000

    Update stuff

---
+++ build/genhash.py
@@ -0,0 +1,14 @@
+import re
+from hashlib import sha256
+
+f = open('update.rdf', 'r')
+u = ''.join(f.readlines())
+f.close()
+
+
+h = sha256(open('anticontainer.xpi', 'rb').read()).hexdigest()
+u = re.sub(r'"sha256:.*?"', r'"sha256:%s"' % h, u)
+
+o = open('update.rdf', 'wb')
+o.write(u)
+o.close()
