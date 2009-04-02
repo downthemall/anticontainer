57abc732cefc32c8ebe3e628458792d47de29e9e
Nils Maier <maierman@web.de>
2009-04-02 20:08:46 +0000

    build scripts

--- build/json-to-filters.py
+++ build/json-to-filters.py
@@ -5,13 +5,14 @@ import simplejson as json
 from glob import glob
 
 filters = ()
-for js in glob("plugins/*.json"):
+for js in glob("../plugins/*.json"):
         f = open(js, 'r')
         js = json.load(f, "utf-8")
         filters += js['match'],
         f.close()
 
 filters = mergeex(map(lambda x: re.sub(r'/', '\\/', x), filters))
+
 print r'pref("extensions.dta.filters.deffilter-ca.label", "AntiContainer");'
 print r'pref("extensions.dta.filters.deffilter-ca.test", "/' +  filters + '/i");'
 print r'pref("extensions.dta.filters.deffilter-ca.active", true);'
