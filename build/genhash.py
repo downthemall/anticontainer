f668d081659d4a4ba63f7af3b4f0c37bb84ec7e6
Nils Maier <maierman@web.de>
2009-04-02 21:20:35 +0000

    more fixes

--- build/genhash.py
+++ build/genhash.py
@@ -9,6 +9,8 @@ f.close()
 h = sha256(open('anticontainer.xpi', 'rb').read()).hexdigest()
 u = re.sub(r'"sha256:.*?"', r'"sha256:%s"' % h, u)
 
+print h
+
 o = open('update.rdf', 'wb')
 o.write(u)
 o.close()
