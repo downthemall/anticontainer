9ef26f373bf083953db01ae2506adb2727a64871
Nils Maier <maierman@web.de>
2009-04-16 04:09:00 +0000

    Restore show source functionality

--- chrome/content/prefpane.js
+++ chrome/content/prefpane.js
@@ -75,13 +75,13 @@ var acPlugins = {
 			date = zeropad(date.getUTCFullYear(), 4)
              + "/" + zeropad(date.getUTCMonth() + 1, 2)
              + "/" + zeropad(date.getUTCDate(), 2);
-			plugs.push([f.prefix, f.id, date , p.indexOf(f.id) != -1, f.priority, f.match, f.type, f.managed, f.author]);
+			plugs.push([f.prefix, f.id, date , p.indexOf(f.id) != -1, f.priority, f.match, f.type, f.managed, f.author, f.file.path]);
 		}
 		plugs.sort(
 			function(a,b) { return a[0] < b[0] ? -1 : (a[0] > b[0] ? 1 : 0);}
 		);
 		let i = 1;
-		for each (let [prefix, plugin, date, disabled, prio, match, ptype, managed, author] in plugs) {
+		for each (let [prefix, plugin, date, disabled, prio, match, ptype, managed, author, file] in plugs) {
 			let li = document.createElement('richlistitem');
 			if (!author) {
 				author = _(managed ? 'ac-syspluginauthor' : 'ac-unkpluginauthor');
@@ -96,6 +96,7 @@ var acPlugins = {
 			li.setAttribute('author', author);
 			li.setAttribute('activated', !disabled);
 			li.setAttribute('managed', managed);
+			li.setAttribute('file', file);
 			this._list.appendChild(li);
 			++i;
 		};
