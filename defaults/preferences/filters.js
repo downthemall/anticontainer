244e39e6760b0efa70e897c36f67c6116a32363e
Nils Maier <maierman@web.de>
2009-04-20 21:39:24 +0000

    Stub filter.js

---
+++ defaults/preferences/filters.js
@@ -0,0 +1,4 @@
+pref("extensions.dta.filters.deffilter-ac.label", "AntiContainer");
+pref("extensions.dta.filters.deffilter-ac.test", "");
+pref("extensions.dta.filters.deffilter-ac.active", true);
+pref("extensions.dta.filters.deffilter-ac.type", 1);
