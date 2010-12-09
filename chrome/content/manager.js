de41a4fa4190c4ece0b18e7a726cb96189a5a2bc
Patrick Westerhoff <PatrickWesterhoff@gmail.com>
2010-12-09 18:19:28 +0100

    Enable `addDownload` in sandbox.

--- chrome/content/manager.js
+++ chrome/content/manager.js
@@ -508,6 +508,9 @@ acResolver.prototype = {
 			function setURL(url) {
 				tp.setURL(maybeWrap(url));
 			}
+			function addDownload(url) {
+				tp.addDownload(url);
+			}
 			function markGone(code, status) {
 				tp.markGone(
 					maybeWrap(code),
@@ -534,6 +537,7 @@ acResolver.prototype = {
 			}
 
 			sb.importFunction(setURL);
+			sb.importFunction(addDownload);
 			sb.importFunction(markGone);
 			sb.importFunction(finish);
 			sb.importFunction(process);
