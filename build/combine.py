0787a2d5d5697e744fcdee4d204b1955d7daf9d1
Nils Maier <maierman@web.de>
2011-11-08 08:52:31 +0100

    sort plugins.json by contained file

--- build/combine.py
+++ build/combine.py
@@ -5,18 +5,22 @@ try:
 except:
     from json import load as json
     from json import dumps as dump
+try:
+    from natsorted import natcasesorted as nsorted
+except:
+    nsorted = sorted
 from glob import glob
 
 VERSION = 0.1
 
 all = []
-    
-for p in glob("../plugins/*.json"):
+
+for p in nsorted(glob("../plugins/*.json")):
     fp = open(p, "r")
     x = json(fp, "utf-8")
     x['date'] = int(os.path.getmtime(p) * 1000)
     fp.close()
     all += x,
-    
+
 fp = open("../modules/plugins.json", "w")
 fp.write(dump(all))
