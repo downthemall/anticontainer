f0e7bdc2992ec9357a4a2731cd7237da4aa4b761
Nils Maier <maierman@web.de>
2013-08-29 14:22:58 +0200

    Improve imgur plugins

---
+++ plugins/sandboxes/imgur.com.resolve.js
@@ -0,0 +1,21 @@
+(function() {
+	try {
+		if (~responseText.indexOf("<h1>File not found!</h1>")) {
+			markGone("File not found!");
+			return;
+		}
+		var url = responseText.match(/rel="image_src" href="(.+?)"/i)[1];
+		var name = responseText.match(/id=['"]image-title['"]>(.*?)</) || null;
+		if (name) {
+			var ext = url.replace(/\?.*$/, "").match(/\.[\w\d+]+$/);
+			name = name[1] + ((ext && ext[0]) || ".jpg");
+		}
+		if (!~url.indexOf("?")) {
+			url = url + "?download";
+		}
+		setURL(url, name);
+	}
+	finally {
+		finish();
+	}
+})();
