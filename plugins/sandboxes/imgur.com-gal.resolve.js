c99986afb0dfb7f6e82f925a051e41cd2340b821
Nils Maier <maierman@web.de>
2015-09-20 12:26:28 +0200

    Fix imgur galleries

--- plugins/sandboxes/imgur.com-gal.resolve.js
+++ plugins/sandboxes/imgur.com-gal.resolve.js
@@ -1,7 +1,7 @@
 try {
-	var images = responseText.match(/images\s+:\s+(\{"count":(?:[\r\n]|.)+?}]}),$/m);
+	var images = responseText.match(/image\s+:\s+({"id".*),$/m);
 	images = JSON.parse(images[1]);
-	for (var i of images.items) {
+	for (var i of images.album_images.images) {
 		queueDownload("/" + i.hash);
 	}
 	setURL(null);
