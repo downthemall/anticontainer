e9191e685bf452f96abb54eaba415379ce6bc3cd
marianocarrazana <marianocarrazana@gmail.com>
2016-03-25 05:01:07 -0300

    Added some plugins and fixed issues

---
+++ plugins/sandboxes/chronos.to.process.js
@@ -0,0 +1,20 @@
+var http = new XMLHttpRequest();
+var url = baseURL;
+var p1 = new RegExp("\.to/(.+)");
+var m1 = p1.exec(baseURL);
+var params = "op=view&id="+m1[1]+"&pre=30&next=Continue+to+image.";
+http.open("POST", url, true);
+http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+http.setRequestHeader("Content-Length", params.length);
+http.onload = function() {
+	var p = new RegExp("src=\"(.+?)\" class=\"pic\" alt=\"(.+?)\"");
+	var m = p.exec(http.responseText);
+	if (m && m.length >= 3) setURL(m[1],m[2]);
+	else markGone();
+	finish();
+}
+http.onerror = function() {
+	markGone();
+	finish();
+}
+http.send(params);
--- plugins/sandboxes/tumblr.com-post.resolve.js
+++ plugins/sandboxes/tumblr.com-post.resolve.js
@@ -1,14 +1,37 @@
 (function() {
-	try {
-		var url = responseText.match(/<meta property="og:image" content="(.+?)"/)[1];
-		var name = responseText.match(/<meta property="og:description" content="(.*?)"/);
-		if (name && name[1].length) {
-			var ext = url.replace(/\?.*$/, "").match(/\.[\w\d+]+$/);
-			name = name[1] + ((ext && ext[0]) || ".jpg");
-		}
-		setURL(url, name);
-	}
-	finally {
-		finish();
-	}
+    try {
+        var ogI = responseText.match(/<meta property="og:image" content="(.+?)"/i),
+            type = responseText.match(/<meta property="og:type" content="(?:.+?:)?(.+?)"/i)[1],
+            obj = responseText.match(/<script.*?type="application\/ld\+json">(.+?)<\/script>/i),
+            url = null,
+            name = null;
+        if(!!obj && !!obj[1]) {
+            obj = JSON.parse(obj[1]);
+            var obT = typeof obj.image;
+            if(obT === "string") {
+                queueDownload(obj.image);
+            }
+            else if(obT === "object") {
+                for(var i of obj.image["@list"])
+                    queueDownload(i);
+            }
+            if(type === "video") {
+                name = "tumblr_" + ogI[1].match(/\/tumblr_([a-zA-Z0-9]+)_frame/)[1];
+                url = "https://www.tumblr.com/video_file/" + baseURL.match(/\/post\/([0-9]+)/)[1] + "/" + name;
+                name = name + ".mp4";
+            }
+            else {
+                throw new Error("Media not located in object.");
+            }
+        }
+        else if(!!ogI && !!ogI[1]) {
+            url = ogI[1];
+        }
+        else {
+            throw new Error("Media not located in page.");
+        }
+        setURL(url, name);
+    }
+    catch (e) { log(e.message); }
+    finally { finish(); }
 })();
