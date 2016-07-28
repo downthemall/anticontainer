78cbf3e63993796105a9e7ce3e3aba48a6bf290e
MegaScience <megascientifical@yahoo.com>
2016-07-28 11:39:05 -0400

    Fix for _r# Tumblr URLs
    Noticed that _r# Tumblr URLs didn't work, so this code should fix that. Also VERY slight adjustments.

--- plugins/sandboxes/tumblr.com-post.resolve.js
+++ plugins/sandboxes/tumblr.com-post.resolve.js
@@ -1,36 +1,32 @@
 (function() {
   "use strict";
   try {
-    var ogI = responseText.match(
-      /<meta property="og:image" content="(.+?)"/i);
-    var type = responseText.match(
-      /<meta property="og:type" content="(?:.+?:)?(.+?)"/i)[1];
-    var obj = responseText.match(
-      /<script.*?type="application\/ld\+json">(.+?)<\/script>/i);
+    var ogI = responseText.match(/<meta property="og:image" content="(.+?)"/i);
+    var type = responseText.match(/<meta property="og:type" content="(?:.+?:)?(.+?)"/i)[1];
+    var obj = responseText.match(/<script.*?type="application\/ld\+json">(.+?)<\/script>/i);
     var url = null;
     var name = null;
-    if (!!obj && !!obj[1]) {
+    if(!!obj && !!obj[1]) {
       obj = JSON.parse(obj[1]);
       var obT = typeof obj.image;
-      if (obT === "string") {
+      if(obT === "string") {
         queueDownload(obj.image);
       }
-      else if (obT === "object") {
-        for (var i of obj.image["@list"]) {
+      else if(obT === "object") {
+        for(var i of obj.image["@list"]) {
           queueDownload(i);
         }
       }
-      if (type === "video") {
-        name = "tumblr_" + ogI[1].match(/\/tumblr_([a-zA-Z0-9]+)_frame/)[1];
-        url = "https://www.tumblr.com/video_file/" +
-              baseURL.match(/\/post\/([0-9]+)/)[1] + "/" + name;
+      if(type === "video") {
+        name = ogI[1].match(/\/(tumblr_[a-zA-Z\d]+)(?:_r\d+)?_frame/)[1];
+        url = "https://www.tumblr.com/video_file/" + baseURL.match(/\/post\/(\d+)/)[1] + "/" + name;
         name = name + ".mp4";
       }
       else {
-          throw new Error("Media not located in object.");
+        throw new Error("Media not located in object.");
       }
     }
-    else if (!!ogI && !!ogI[1]) {
+    else if(!!ogI && !!ogI[1]) {
       url = ogI[1];
     }
     else {
@@ -38,10 +34,6 @@
     }
     setURL(url, name);
   }
-  catch (e) {
-    log(e.message);
-  }
-  finally {
-    finish();
-  }
+  catch (e) { log(e.message); }
+  finally { finish(); }
 })();
